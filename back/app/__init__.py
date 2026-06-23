5572947245ae60af24f6b2b7bd107c625d01b0b7
sdhuen01-del <sdhuen01@gmail.com>
2026-06-23 09:41:26 +0900

    20260623

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -12,8 +12,8 @@ jwt     = JWTManager()
 
 def create_app():
     app = Flask(__name__,
-                template_folder='../front/templates',
-                static_folder='../front/static')
+            template_folder='../../front/templates',
+            static_folder='../../front/static')
     app.config.from_object('config.Config')
 
     db.init_app(app)
