ff343def9769c6f9616d7e5bd82be4bf0ab41599
sdhuen01-del <sdhuen01@gmail.com>
2026-06-24 17:36:43 +0900

    260505

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'   
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 👈 Tailwind v4용 공식 플러그인 추가
+    tailwindcss()  
   ],
   server: {
     port: 5173,
